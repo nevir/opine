edf097ee99a9547861763a570b5b397e3baac3fd
Ian <ian@nevir.net>
2016-02-27 10:00:20 -0800

    Be clearer about extension

--- base.js
+++ base.js
@@ -2,7 +2,7 @@ const opine = require('opine');
 
 module.exports = class Base extends opine.Model(__dirname, 'base') {
 
-  templateContext() {
+  extraContext() {
     return {
       nodeVersion: '4.3.1',
       npmVersion: '3.7.2',
