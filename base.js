f2e8f1e4d9540946059478078f0be7919afe04bd
Ian <ian@nevir.net>
2016-02-27 10:00:43 -0800

    Line those vars up

--- base.js
+++ base.js
@@ -5,7 +5,7 @@ module.exports = class Base extends opine.Model(__dirname, 'base') {
   extraContext() {
     return {
       nodeVersion: '4.3.1',
-      npmVersion: '3.7.2',
+      npmVersion:  '3.7.2',
     };
   }
 
