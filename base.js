b4ee855ea5066884aa63e11d944f2e3fa1d1608e
Ian <ian@nevir.net>
2016-02-27 09:51:35 -0800

    Node and npm versions, and an example of template context declaration

--- base.js
+++ base.js
@@ -1,1 +1,12 @@
-module.exports = require('opine').Model(__dirname, 'base');
+const opine = require('opine');
+
+module.exports = class Base extends opine.Model(__dirname, 'base') {
+
+  templateContext() {
+    return {
+      nodeVersion: '4.3.1',
+      npmVersion: '3.7.2',
+    };
+  }
+
+}
